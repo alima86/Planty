7ac90ffd51f2e26c35fa08cc821d41d66a950cfe
alima86 <alima.setta@yahoo.fr>
2024-02-05 16:12:27 +0100

    QuatriemeEnvoi

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -6,4 +6,12 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
+ add_filter('wp_nav_menu_items', 'custom_menu_admin_link');
+ function custom_menu_admin_link($items) {
+     if (is_user_logged_in() && has_nav_menu('navigation')) {
+         $items .= '<li><a href="' . admin_url() . '">Admin</a></li>';
+     }
+     return $items;
+ }
+
 }

bff9dd57b1248fb56f3d72d8f62270bc68c1c1be
alima86 <alima.setta@yahoo.fr>
2024-02-10 17:15:13 +0100

    CinquiemeEnvoi

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -5,13 +5,12 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+}
  add_filter('wp_nav_menu_items', 'custom_menu_admin_link');
  function custom_menu_admin_link($items) {
-     if (is_user_logged_in() && has_nav_menu('navigation')) {
+   $menu = wp_get_nav_menu_object( 3 );
+     if (is_user_logged_in() && $menu->name == 'Navigation' )  {
          $items .= '<li><a href="' . admin_url() . '">Admin</a></li>';
      }
      return $items;
  }
-
-}

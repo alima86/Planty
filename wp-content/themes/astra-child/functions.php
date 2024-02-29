0a201f39ff866fc02c1c77c64bcc3dce95aeb696
alima86 <alima.setta@yahoo.fr>
2024-02-29 17:17:11 +0100

    SeptiemeEnvoi

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -6,11 +6,13 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
- add_filter('wp_nav_menu_items', 'custom_menu_admin_link');
- function custom_menu_admin_link($items) {
-   $menu = wp_get_nav_menu_object( 3 );
-     if (is_user_logged_in() && $menu->name == 'Navigation' )  {
-         $items .= '<li><a id="admin-link" href="' . admin_url() . '">Admin</a></li>';
-     }
-     return $items;
- }
+/**
+** activation admin
+**/
+add_filter('wp_nav_menu_items', 'custom_menu_admin_link', 10, 2 );
+function custom_menu_admin_link($items,$args) {
+    if (is_user_logged_in() && $args->menu == 'navigation' )  {
+$items .= '<li class="menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu"> <a id="admin-link" class="hfe-menu-item" href="' . admin_url() . '">Admin</a></li>';
+}
+    return $items;
+}
